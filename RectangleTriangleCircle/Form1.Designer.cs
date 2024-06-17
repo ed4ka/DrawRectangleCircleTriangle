e6b9b9b306a383df0768f8273393b55b74e20f17
Rafi <ifreshgalaxy@gmail.com>
2024-06-17 10:04:04 +0300

    Add project files.

---
+++ RectangleTriangleCircle/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RectangleTriangleCircle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnRed.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRed.Location = new System.Drawing.Point(12, 12);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(134, 34);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "RECTANGLE";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click_1);
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnTriangle.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTriangle.Location = new System.Drawing.Point(152, 12);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(126, 34);
+            this.btnTriangle.TabIndex = 1;
+            this.btnTriangle.Text = "TRIANGLE";
+            this.btnTriangle.UseVisualStyleBackColor = false;
+            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click_1);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCircle.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCircle.Location = new System.Drawing.Point(284, 12);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(126, 34);
+            this.btnCircle.TabIndex = 2;
+            this.btnCircle.Text = "CIRCLE";
+            this.btnCircle.UseVisualStyleBackColor = false;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnStop.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(416, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(126, 34);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 307);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnTriangle);
+            this.Controls.Add(this.btnRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ RectangleTriangleCircle/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RectangleTriangleCircle
+{
+    public partial class Form1 : Form
+    {
+
+        private Random rdm;
+        private Thread rectangleThread;
+        private Thread triangleThread;
+        private Thread circleThread;
+        private bool isRunning; // Флаг за продължаване на генерирането
+
+        public Form1()
+        {
+            InitializeComponent();
+            rdm = new Random();
+            isRunning = false;
+        }
+
+
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                circleThread = new Thread(GenerateCircles);
+                circleThread.Start();
+            }
+        }
+
+        private void GenerateRectangles()
+        {
+            try
+            {
+                while (isRunning)
+                {
+                    int width = rdm.Next(10, 100); 
+                    int height = rdm.Next(10, 100); 
+                    int x = rdm.Next(0, this.ClientSize.Width - width);
+                    int y = rdm.Next(0, this.ClientSize.Height - height);
+
+                    using (Graphics g = this.CreateGraphics())
+                    {
+                        g.DrawRectangle(new Pen(Brushes.Red, 4), new Rectangle(x, y, width, height));
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                Thread.ResetAbort();
+            }
+        }
+
+
+        private void GenerateTriangles()
+        {
+            try
+            {
+                while (isRunning)
+                {
+                    int size = rdm.Next(10, Math.Min(this.ClientSize.Width, this.ClientSize.Height) / 2);
+                    int x = rdm.Next(0, this.ClientSize.Width - size);
+                    int y = rdm.Next(0, this.ClientSize.Height - size);
+
+                    Point[] points = {
+                        new Point(x, y + size),
+                        new Point(x + size, y + size),
+                        new Point(x + size / 2, y)
+                    };
+
+                    using (Graphics g = this.CreateGraphics())
+                    {
+                        g.DrawPolygon(new Pen(Brushes.Green, 4), points);
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                Thread.ResetAbort();
+            }
+        }
+
+        private void GenerateCircles()
+        {
+            try
+            {
+                while (isRunning)
+                {
+                    int diameter = rdm.Next(10, Math.Min(this.ClientSize.Width, this.ClientSize.Height) / 2);
+                    int x = rdm.Next(0, this.ClientSize.Width - diameter);
+                    int y = rdm.Next(0, this.ClientSize.Height - diameter);
+
+                    using (Graphics g = this.CreateGraphics())
+                    {
+                        g.DrawEllipse(new Pen(Brushes.Blue, 4), new Rectangle(x, y, diameter, diameter));
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                Thread.ResetAbort();
+            }
+        }
+
+
+        private void btnRed_Click_1(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                rectangleThread = new Thread(GenerateRectangles);
+                rectangleThread.Start();
+            }
+        }
+
+        private void btnTriangle_Click_1(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                triangleThread = new Thread(GenerateTriangles);
+                triangleThread.Start();
+            }
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            isRunning = false;
+
+            if (rectangleThread != null && rectangleThread.IsAlive)
+            {
+                rectangleThread.Abort();
+            }
+
+            if (triangleThread != null && triangleThread.IsAlive)
+            {
+                triangleThread.Abort();
+            }
+
+            if (circleThread != null && circleThread.IsAlive)
+            {
+                circleThread.Abort();
+            }
+
+            using (Graphics g = this.CreateGraphics())
+            {
+                g.Clear(BackColor);
+            }
+        }
+    }
+}
